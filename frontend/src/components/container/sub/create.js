16ff94f765399813899cfffa088f5b56532c4b9a
Azim Gadzhiagayev <me@azimgd.com>
2016-01-31 14:26:08 +0100

    ADD configuration for create container component

--- frontend/src/components/container/sub/create.js
+++ frontend/src/components/container/sub/create.js
@@ -12,11 +12,19 @@ class Create extends Component {
       Name: null,
       Cmd: [],
       Env: [],
+      Mounts: [],
     };
   }
 
   handleChange(type, event) {
-    const data = (type === 'string') ? event.target.value : [].concat([event.target.value]);
+    const value = event.target.value;
+    const data = (type === 'string') ? value : this.state[event.target.name].concat([value]);
+
+    if(!value || 0 === value.length) {
+      return;
+    } else {
+      event.target.value = "";
+    }
 
     this.setState({
       [event.target.name]: data,
@@ -27,20 +35,50 @@ class Create extends Component {
     this.props.createContainer(this.state);
   }
 
+  removeEntry(field, position, event) {
+    event.preventDefault();
+    const array = this.state[field];
+    array.slice(position, 1);
+
+    this.setState({
+      field: array,
+    });
+  }
+
   render() {
+    const mountsFormat = { "Source": "/data", "Destination": "/data", "Mode": "ro,Z", "RW": false };
     return (
       <div styleName="Form">
         <div styleName="Form-group">
-          <input type="text" name="Image" placeholder="Image" styleName="Form-control" defaultValue={this.state.Image} onChange={this.handleChange.bind(this, 'string')}/>
+          <label>Image</label>
+          <input type="text" name="Image" placeholder="Image" styleName="Form-control" defaultValue={this.state.Image} onBlur={this.handleChange.bind(this, 'string')}/>
         </div>
         <div styleName="Form-group">
-          <input type="text" name="Name" placeholder="Name" styleName="Form-control" defaultValue={this.state.Name} onChange={this.handleChange.bind(this, 'string')}/>
+          <label>Name</label>
+          <input type="text" name="Name" placeholder="Name" styleName="Form-control" defaultValue={this.state.Name} onBlur={this.handleChange.bind(this, 'string')}/>
         </div>
         <div styleName="Form-group">
-          <input type="text" name="Cmd" placeholder="Cmd" styleName="Form-control" defaultValue={this.state.Cmd} onChange={this.handleChange.bind(this, 'array')}/>
+          <label>Cmd</label>
+          {this.state.Cmd.map((item, key) =>
+            <div key={key}>{item} <a href="" onClick={this.removeEntry.bind(this, 'Cmd', key)}>Remove</a></div>
+          )}
+          <input type="text" name="Cmd" placeholder="Cmd" styleName="Form-control" defaultValue={this.state.Cmd} onBlur={this.handleChange.bind(this, 'array')}/>
         </div>
         <div styleName="Form-group">
-          <input type="text" name="Env" placeholder="Env" styleName="Form-control" defaultValue={this.state.Env} onChange={this.handleChange.bind(this, 'array')}/>
+          <label>Env</label>
+          {this.state.Env.map((item, key) =>
+            <div key={key}>{item} <a href="" onClick={this.removeEntry.bind(this, 'Env', key)}>Remove</a></div>
+          )}
+          <input type="text" name="Env" placeholder="Env" styleName="Form-control" defaultValue={this.state.Env} onBlur={this.handleChange.bind(this, 'array')}/>
+          <small>"FOO=bar"</small>
+        </div>
+        <div styleName="Form-group">
+          <label>Mounts</label>
+          {this.state.Mounts.map((item, key) =>
+            <div key={key}>{item} <a href="" onClick={this.removeEntry.bind(this, 'Mounts', key)}>Remove</a></div>
+          )}
+          <input type="text" name="Mounts" placeholder="Mounts" styleName="Form-control" defaultValue={this.state.Mounts} onBlur={this.handleChange.bind(this, 'array')}/>
+          <small>{JSON.stringify(mountsFormat)}</small>
         </div>
 
         <button styleName="Btn" onClick={this.handleSubmit.bind(this)}>Create container</button>
